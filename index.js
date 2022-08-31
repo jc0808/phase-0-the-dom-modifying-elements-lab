ae8731fd3cb0cec755fed49b97f3a41fa2649da6
Joshua Campos <jdeveloper1203@gmail.com>
2022-08-31 17:10:55 -0400

    commit tree 1a18d00676a98c513f774a1726570f7f307c2e83

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+
+main.remove();
+
+const newHeader = document.createElement('H1');
+
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Joshua is the champion";
+
+document.append(newHeader);
